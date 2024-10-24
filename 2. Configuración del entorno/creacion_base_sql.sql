f75957af44f6b7188460271ea8886b56e343a1c5
26-jorge-01 <jonan0804@gmail.com>
2024-10-24 07:59:05 +0200

    clase 3: Modelo de datos en SQL, despliegue e interacción con el contrato desde python

--- 2. Configuración del entorno/creacion_base_sql.sql
+++ 2. Configuración del entorno/creacion_base_sql.sql
@@ -4,18 +4,23 @@ CREATE DATABASE VotingSystem;
 -- Cambia el contexto a la base de datos recién creada 'VotingSystem'
 USE VotingSystem;
 
--- Crea una tabla llamada 'Votes' en la base de datos 'VotingSystem'
-CREATE TABLE Votes (
-    id INT PRIMARY KEY IDENTITY,           -- 'id': columna de tipo entero (INT), actúa como clave primaria. 
-                                           -- Utiliza 'IDENTITY' para que se incremente automáticamente con cada nueva fila.
-    voter_id VARCHAR(255) NOT NULL,        -- 'voter_id': columna de tipo cadena de texto (VARCHAR) con un límite de 255 caracteres.
-                                           -- Almacena la identificación del votante. La columna es obligatoria (NOT NULL).
-    candidate VARCHAR(255) NOT NULL,       -- 'candidate': columna de tipo cadena de texto (VARCHAR) con un límite de 255 caracteres.
-                                           -- Almacena el nombre del candidato por el cual el votante ha votado. Es obligatoria (NOT NULL).
-    timestamp DATETIME DEFAULT GETDATE()   -- 'timestamp': columna de tipo fecha y hora (DATETIME). 
-                                           -- Almacena la fecha y hora en que se registró el voto. Tiene un valor por defecto (DEFAULT) 
-                                           -- que usa la fecha y hora actual del sistema (`GETDATE()`).
+CREATE TABLE Candidatos (
+    ID INT PRIMARY KEY,
+    Nombre VARCHAR(50)
+);
+
+CREATE TABLE Votantes (
+    ID INT PRIMARY KEY,
+    Nombre VARCHAR(50)
+);
+
+CREATE TABLE Votos (
+    ID INT PRIMARY KEY,
+    ID_votante INT,
+    ID_candidato INT,
+    FOREIGN KEY (ID_votante) REFERENCES Votantes(ID),
+    FOREIGN KEY (ID_candidato) REFERENCES Candidatos(ID)
 );
 
 -- "Trae toda la informacion de la tabla Votes"
-SELECT * FROM Votes;
+SELECT * FROM Votos;
